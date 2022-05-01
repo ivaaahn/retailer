90044fcf6229f1a8b36b6a386a282527cb50bd3b
Dmitry Ivakhnenko <ivahnencko01@gmail.com>
2022-05-01 22:50:12 +0300

    [feature/DBC-22]: Django was added (#18)
    Co-authored-by: IrinaBarkalova <i_barkalova@mail.ru>

---
+++ retailer_admin/shop_addresses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from users.models import UserModel
+
+
+class ShopAddressModel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    city = models.TextField(verbose_name="Город", null=False, blank=False)
+    street = models.TextField(verbose_name="Улица", null=False, blank=False)
+    house = models.TextField(verbose_name="Дом", null=False, blank=False)
+    floor = models.IntegerField(verbose_name="Этаж", null=True, blank=True)
+
+    def __str__(self):
+        return f"Адрес: {self.city}, {self.street}, {self.house}"
+
+    class Meta:
+        managed = False
+        db_table = "shop_addresses"
+        verbose_name = "Адрес магазина"
+        verbose_name_plural = "Адреса магазинов"
