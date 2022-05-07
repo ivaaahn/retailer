971b6eb81e934b00fc9d5ee5164831e166e286c2
d_ivahnencko <ivahnencko01@gmail.com>
2022-05-07 22:50:56 +0300

    feature/DBC-31: Redis for cart and products was added

--- retailer/app/delivery/cart/errors.py
+++
@@ -1,8 +0,0 @@
-from app.base.errors import ConflictError
-
-
-class ShopAmbiguity(ConflictError):
-    def __init__(self):
-        super().__init__(
-            description="В корзине есть продукты из разных магазинов",
-        )
