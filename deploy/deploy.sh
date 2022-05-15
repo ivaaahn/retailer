ae12d758de88450ded02e40d713fc79017abd5e6
d_ivahnencko <ivahnencko01@gmail.com>
2022-05-15 18:41:02 +0300

    [hf]: Fixed deploy script and nginx.conf

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
-echo $1
-echo $2
 retailer_tag=$1
 retailer_admin_tag=$2
 
 echo "  [>] Starting deployment"
 
 echo "  [+] Remove containers, volume and networks older than 1 week..."
-docker system prune --force --filter "until=168h"
+docker system prune --force --filter "until=1h"
 
 # shellcheck disable=SC2164
 cd ~/retailer/deploy/
@@ -23,6 +21,5 @@ echo "  [+]  Retailer-Admin Version: $retailer_admin_tag"
 
 
 echo "  [+] Start (or Restart) containers: docker-compose up -d"
-docker-compose --env-file /home/www/retailer/deploy/.env.docker-compose -f /home/www/retailer/deploy/docker-compose.yml up -d
-
-echo "[>] Deployment done."
+@@@
+                                                                                             1,1           Top
