78826423e0b9228a22143d676aa703e63e3f5cfe
Dmitry Ivakhnenko <ivahnencko01@gmail.com>
2022-05-15 18:45:53 +0300

    Dev (#38)
    * [hf]: Fixed deploy script and nginx.conf

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
-echo $1
-echo $2
 retailer_tag=$1
 retailer_admin_tag=$2
 
 echo "  [>] Starting deployment"
 
 echo "  [+] Remove containers, volume and networks older than 1 week..."
-docker system prune --force --filter "until=168h"
+docker system prune --force --filter "until=1h"
 
 # shellcheck disable=SC2164
 cd ~/retailer/deploy/
