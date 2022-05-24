c98b95ca707f619f1c19b26a8b1a2b59624c1596
Dmitry Ivakhnenko <ivahnencko01@gmail.com>
2022-05-25 00:58:52 +0300

    feature/DBC-36: Add sending email when order placed (#54) (#55)

--- retailer/app/repos/rmq/interface.py
+++ retailer/app/repos/rmq/interface.py
@@ -5,3 +5,7 @@ class IRMQInteractRepo(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     async def send_code(self, email: str, code: str):
         pass
+
+    @abc.abstractmethod
+    async def send_accept(self, email: str, order_id: int):
+        pass
