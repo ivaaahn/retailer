501a48fb9e5bfe47534a1942a29223e4ce838c66
d_ivahnencko <ivahnencko01@gmail.com>
2022-06-05 15:42:27 +0300

    hf/deployment: update

--- deploy/down.sh
+++ deploy/down.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 echo "[>] Starting downing"
+
 # shellcheck disable=SC2164
 cd ~/retailer/deploy/
 docker-compose --env-file /home/www/retailer/deploy/.env.docker-compose down
+
 echo "[>] Downing done."
