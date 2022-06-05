d682512b046ee23c193bf847b664d098694ca1e0
d_ivahnencko <ivahnencko01@gmail.com>
2022-06-05 15:56:34 +0300

    hf/deployment: update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-retailer_tag=$1
-retailer_admin_tag=$2
-
 echo "  [>] Starting deployment"
 
 echo "  [+] Remove containers, volume and networks older than 1 week..."
@@ -11,13 +8,15 @@ docker system prune --force --filter "until=1h"
 # shellcheck disable=SC2164
 cd ~/retailer/deploy/
 
-export RETAILER_VERSION=$retailer_tag
-export RETAILER_ADMIN_VERSION=$retailer_admin_tag
+export RETAILER_VERSION=$1
+export RETAILER_ADMIN_VERSION=$2
 export PYMAILER_VERSION=0.1
 export RABBITMQ_VERSION=0.1
 
-echo "  [+]  Retailer Version: : $retailer_tag"
-echo "  [+]  Retailer-Admin Version: $retailer_admin_tag"
+echo "  [+]  Retailer Version: : $RETAILER_VERSION"
+echo "  [+]  Retailer-Admin Version: $RETAILER_ADMIN_VERSION"
+echo "  [+]  Pymailer Version: $PYMAILER_VERSION"
+echo "  [+]  RabbitMQ Version: $RABBITMQ_VERSION"
 
 
 echo "  [+] Start (or Restart) containers: docker-compose up -d"
