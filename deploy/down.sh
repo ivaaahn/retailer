b36b7276955be0fb83a78e5800379faca0b0153f
d_ivahnencko <ivahnencko01@gmail.com>
2022-08-28 15:31:33 +0300

    hf: fixed db function signature

--- deploy/down.sh
+++ deploy/down.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
 
-echo "[>] Starting downing"
+echo "[>>>] Downing..."
 
-# shellcheck disable=SC2164
-cd ~/retailer/deploy/
+cd ~/retailer/deploy/ || exit
 
-echo "[>] docker-compose down..."
-docker-compose --env-file /home/www/retailer/deploy/.env.docker-compose down
+echo "[>] Docker compose down..."
+docker compose down
 
-
-echo "[>] docker stop all containers"
+echo "[>] Docker stop all containers..."
 # shellcheck disable=SC2046
 docker stop $(docker ps -a -q)
 
-echo "[>] docker remove all containers"
+echo "[>] Docker remove all containers..."
 # shellcheck disable=SC2046
 docker rm $(docker ps -a -q)
 
-echo "[>] docker remove images retailer and retailer-admin"
+echo "[>] Docker remove images retailer and retailer-admin"
 docker rmi ivaaahn/retailer:latest
 docker rmi ivaaahn/retailer-admin:latest
 
