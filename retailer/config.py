29e34f9f372f3e7dd896e885d80a53f8f9bb26fc
ivaaahn <ivahnencko01@gmail.com>
2022-11-30 19:17:29 +0300

    add cicd

--- retailer/config.py
+++ retailer/config.py
@@ -2,16 +2,16 @@ import os
 
 from pydantic import BaseSettings
 
-DEPLOY_MODE = os.environ.get("DEPLOY_MODE", False)
+RUN_MODE = os.environ.get("RUN_MODE", "local")
 
 dotenv_files = {
     "deploy": ".env.deploy",
     "test": ".env.test",
+    "local": ".env.local",
 }
 
 
 class RetailerConfig(BaseSettings):
     class Config:
-        env_file = (
-            dotenv_files["deploy"] if DEPLOY_MODE else dotenv_files["test"]
-        )
+        env_file = dotenv_files[RUN_MODE]
+
